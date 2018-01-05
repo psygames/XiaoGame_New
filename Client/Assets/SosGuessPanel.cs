a4f763b35d2d55d96bc69c7811815122d11fdb16
yinlong <85171642@163.com>
2018-01-05 10:40:59 +0800

    添加Panel部分代码

---
+++ Client/Assets/SosGuessPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RedStone.UI;
+
+namespace RedStone
+{
+    public class SosGuessPanel : MonoBehaviour
+    {
+    }
+}
